07e65580931a6d20fed3dcdd4b7d6cb4583fcec8
Akinni James <akinnijames@live.com>
2018-12-12 11:58:36 -0500

    Merge branch 'master' of https://github.com/wolfembers/Pygame-Lib

--- Map.py
+++ Map.py
@@ -1,13 +1,9 @@
 import pygame
 
-
 class Map:
-    def __init__():
+    def __init__(self):
         pass
-<<<<<<< HEAD
 print("Hello")
-=======
 
 
 print("Palmer's Test")
->>>>>>> 3aaf7790ff9e22836d0829fa3f67937597a29886
