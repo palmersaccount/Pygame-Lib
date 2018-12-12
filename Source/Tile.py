4e956bbfc12c83b9fd26ad246a631ab9732719a4
Akinni James <akinnijames@live.com>
2018-12-12 12:27:41 -0500

    tiles 4

--- Source/Tile.py
+++ Source/Tile.py
@@ -4,7 +4,8 @@ class Tile:
     def __init__(self, x, y, w, h, img=False):
         self.X = x
         self.Y = y
-        self.Rect = (x, y, w, h)
+        self.DestinationRect = [x, y, w, h]
+        self.SourceRect = [0, 0, w, h]
         self.UsingImage = img
 
     def draw(self):
