51e854f7c900ae41b1b64ea61851a6844450eafa
Akinni James <akinnijames@live.com>
2018-12-12 12:06:03 -0500

    Update Map.py

--- Map.py
+++ Map.py
@@ -1,12 +1,6 @@
 import pygame
 
-
 class Map:
-<<<<<<< HEAD
     def __init__(self, width, height):
         self.Width = width
         self.Height = height
-=======
-    def __init__():
-        pass
->>>>>>> c67bf2a377b3e8c793f7edb44896d2ef922c2945
