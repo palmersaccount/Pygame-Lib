bd098f295a8b33e823cc9e760fecbf5d6352cf54
Akinni James <akinnijames@live.com>
2018-12-12 20:08:47 -0500

    edits

--- Source/Vector2.py
+++ Source/Vector2.py
@@ -5,46 +5,65 @@ class Vector2:
         self.X = x
         self.Y = y
 
+    # convert to list
+    def tolist(self):
+        return [self.X, self.Y]
+
+    # convert to tuple
+    def totuple(self):
+        return (self.X, self.Y)
+
+    # get the length
+    def length(self):
+        return self.length_squared() ** (1 / 2)
+
+    # get the length squared
+    def length_squared(self):
+        return self.X ** 2 + self.Y ** 2
+
     # override + operator
     def __add__(self, other):
-        pass
+        return Vector2(self.X + other.X, self.X + other.X)
 
     # override * operator
     def __mul__(self, other):
-        pass
+        if isinstance(other, Vector2):
+            return Vector2(self.X * other.X, self.X * other.X)
+        else:
+            return Vector2(self.X * other, self.Y * other)
 
     # override - operator
     def __sub__(self, other):
-        pass
+        return Vector2(self.X - other.X, self.Y - other.Y)
 
     # override / operator
     def __truediv__(self, other):
-        pass
+        return Vector2(self.X / other.X, self.Y / other.Y)
 
     # override == operator
     def __eq__(self, other):
-        pass
+        return self.X == other.X and self.Y == other.Y
 
     # override < operator
     def __lt__(self, other):
-        pass
+        return self.length() < other.length()
 
     # override <= operator
     def __le__(self, other):
-        pass
+        return self.length() <= other.length()
 
     # override >= operator
     def __ge__(self, other):
-        pass
+        return self.length() >= other.length()
 
     # override > operator
     def __gt__(self, other):
-        pass
+        return self.length() > other.length()
 
     # override != operator
     def __ne__(self, other):
-        pass
+        return self.X != self.X or self.Y != self.Y
 
     # override str operator
     def __str__(self):
-        pass
+        return "X = " + str(self.X) + ", Y = " + str(self.Y)
---
+++ Tests/Vectors.py
@@ -0,0 +1,6 @@
+# this file is meant for testing the vectors class
+from Source.Vector2 import Vector2
+
+vec1 = Vector2(5, 5)
+print(vec1)
+print(vec1.length())
